ba7ec5b38d25b02b7c9ac9e63ba98145f4912e38
ym <>
2018-12-05 14:07:57 +0800

    测试项目

---
+++ ccscrollbar/ccscrollbar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ccscrollbar
+//
+//  Created by catcher on 2018/12/5.
+//  Copyright © 2018年 ym. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ccscrollbar/ccscrollbar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ccscrollbar
+//
+//  Created by catcher on 2018/12/5.
+//  Copyright © 2018年 ym. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
